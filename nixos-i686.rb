67e56ace8b29a48d6bd067a99715b72a3d0b3e7a
rodamber <rodrigoambernardo@gmail.com>
2016-05-28 00:51:25 +0100

    Removed unwanted files.

--- nixos-i686.rb
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env ruby
-require './gen_template.rb'
-
-gen_template(
-  arch: "i686",
-  iso_url: "https://nixos.org/releases/nixos/15.09/nixos-15.09.1076.9220f03/nixos-minimal-15.09.1076.9220f03-i686-linux.iso",
-  iso_sha256: "4ffa1cbaa006bd6d804ecbf3da3c613fbf57602e9c383e5d985f2fbd6ff265f5",
-  virtualbox_guest_os: "Linux",
-)
